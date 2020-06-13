521b8ee99e89475013cc1ee419d6bcdb060a1e36
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-06-13 15:16:58 -0300

    Subject: add errors messages validation for email and password input Problem: Solution: Note:

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -11,6 +11,8 @@
             <v-form>
               <v-text-field
                 v-model.trim="$v.user.email.$model"
+                :error-messages="emailErrors"
+                :success="!$v.user.email.$invalid"
                 prepend-icon="email"
                 name="email"
                 label="Email"
@@ -18,10 +20,12 @@
               ></v-text-field>
               <v-text-field
                 v-model.trim="$v.user.password.$model"
+                :error-messages="passwordErrors"
                 prepend-icon="lock"
                 name="password"
                 label="Senha"
                 type="password"
+                counter="6"
               ></v-text-field>
             </v-form>
 
@@ -60,6 +64,28 @@ export default {
         required,
         minLength: minLength(6)
       }
+    }
+  },
+  computed: {
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty) {
+        return errors
+      }
+      !email.required && errors.push('Email obrigatorio')
+      !email.email && errors.push('Insira um email valido')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) {
+        return errors
+      }
+      !password.required && errors.push('Senha obrigatorio')
+      !password.minLength && errors.push('Insira uma senha valido')
+      return errors
     }
   },
   methods: {
