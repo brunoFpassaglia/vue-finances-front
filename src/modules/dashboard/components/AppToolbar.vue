340570845f278d415ded5c17356af23f8cf9c91c
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-06-17 20:27:34 -0300

    Subject: add menu items and hide functionality Problem: Solution: Note:

--- src/modules/dashboard/components/AppToolbar.vue
+++ src/modules/dashboard/components/AppToolbar.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-app-bar app fixed color="primary">
+  <v-app-bar
+    app
+    fixed
+    color="primary"
+  >
     <v-icon @click.stop="$emit('hide', !showMenu)">menu</v-icon>
     <v-toolbar-title>Dashboard</v-toolbar-title>
   </v-app-bar>
