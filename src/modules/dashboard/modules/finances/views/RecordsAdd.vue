156cd7892b15bfa272255518b537b24d9d237da2
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-06-29 22:01:06 -0300

    Subject: add dialog to select due date of the record Problem: Solution: Note:

--- src/modules/dashboard/modules/finances/views/RecordsAdd.vue
+++ src/modules/dashboard/modules/finances/views/RecordsAdd.vue
@@ -22,6 +22,50 @@
           <v-card>
             <v-card-text>
               <v-form>
+
+                <v-dialog
+                  ref="dateDialog"
+                  :return-value.sync="record.date"
+                  v-model="showDateDialog"
+                  persistent
+                  lazy
+                  width="290px"
+                  full-width
+                >
+
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      name="date"
+                      label="Vencimento"
+                      prepend-icon="event"
+                      type="text"
+                      readonly
+                      :value="formattedDate"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+
+                  <v-date-picker
+                    :color="color"
+                    locale="pt-br"
+                    scrollable
+                    v-model="dateDialogValue"
+                  >
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      flat
+                      :color="color"
+                      @click="cancelDateDialog"
+                    >Cancelar</v-btn>
+                    <v-btn
+                      flat
+                      :color="color"
+                      @click="$refs.dateDialog.save(dateDialogValue)"
+                    >Ok</v-btn>
+                  </v-date-picker>
+
+                </v-dialog>
+
                 <v-select
                   label="account"
                   name="Conta"
@@ -29,6 +73,7 @@
                   :items="accounts"
                   item-text="description"
                   item-value="id"
+                  v-model="$v.record.accountId.$model"
                 ></v-select>
                 <v-select
                   label="category"
@@ -37,6 +82,7 @@
                   :items="categories"
                   item-text="description"
                   item-value="id"
+                  v-model="$v.record.categoryId.$model"
                 ></v-select>
 
                 <v-text-field
@@ -44,22 +90,43 @@
                   label="Descricao"
                   prepend-icon="description"
                   type="text"
+                  v-model="$v.record.description.$model"
                 ></v-text-field>
                 <v-text-field
                   name="tags"
                   label="Tags"
                   prepend-icon="label"
                   type="text"
+                  v-model="record.tags"
                 ></v-text-field>
                 <v-text-field
                   name="note"
                   label="Observacao"
                   prepend-icon="note"
                   type="text"
+                  v-model="record.note"
                 ></v-text-field>
               </v-form>
             </v-card-text>
           </v-card>
+          <v-btn
+            color="secondary"
+            large
+            fab
+            class="mt-4"
+            @click="$router.back()"
+          >
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-btn
+            :color="color"
+            large
+            fab
+            class="mt-4"
+            @click="submit"
+          >
+            <v-icon>check</v-icon>
+          </v-btn>
         </v-flex>
 
       </v-layout>
@@ -89,6 +156,23 @@ export default {
         description: '',
         tags: '',
         note: ''
+      },
+      showDateDialog: false,
+      dateDialogValue: moment().format('YYYY-MM-DD')
+    }
+  },
+  computed: {
+    formattedDate () {
+      return moment(this.record.date).format('DD/MM/YYYY')
+    },
+    color () {
+      switch (this.record.type) {
+        case 'credit':
+          return 'primary'
+        case 'debit':
+          return 'error'
+        default:
+          return 'blue'
       }
     }
   },
@@ -125,6 +209,9 @@ export default {
           break
       }
       this.setTitle({ title })
+    },
+    submit () {
+      return 0
     }
   },
   async created () {
