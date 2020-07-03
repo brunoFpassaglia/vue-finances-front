8ec082278d17692a823441c265dd20c6a3e038cc
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-07-03 20:04:21 -0300

    Subject: add form validation to create account & category Problem: Solution: Note:

--- src/modules/dashboard/modules/finances/components/AccountCategoryAdd.vue
+++ src/modules/dashboard/modules/finances/components/AccountCategoryAdd.vue
@@ -1,16 +1,20 @@
 <template>
   <v-card>
-    <v-card-title>{{title}}</v-card-title>
+    <v-card-title class="headline">{{title}}</v-card-title>
     <v-card-text>
-      <v-text-field label="Descricao"></v-text-field>
+      <v-text-field label="Descricao" v-model="$v.item.description.$model"></v-text-field>
       <v-select
         v-if="entity === 'category'"
         label="Operacao"
+        v-model="$v.item.operation.$model"
+        :items="operations"
+        item-text="description"
+        item-value="value"
       ></v-select>
       <v-card-actions>
         <v-spacer>
           <v-btn @click="$emit('close')">Cancelar</v-btn>
-          <v-btn color="primary">Salvar</v-btn>
+          <v-btn color="primary" @click="save" :disabled="$v.$invalid">Salvar</v-btn>
         </v-spacer>
       </v-card-actions>
     </v-card-text>
@@ -19,10 +23,47 @@
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'AccountCategoryAdd',
   props: {
     entity: String
+  },
+  data () {
+    return {
+      item: {
+        description: '',
+        operation: ''
+      },
+      operations: [
+        { description: 'Receita', value: 'CREDIT' },
+        { description: 'Despesa', value: 'DEBIT' }
+      ]
+    }
+  },
+  validations () {
+    const validations = {
+      item: {
+        description: {
+          required,
+          minLength: minLength(3)
+        }
+      }
+    }
+
+    if (this.entity === 'account') { return validations }
+
+    return {
+      item: {
+        ...validations.item,
+        operation: { required }
+      }
+    }
+  },
+  methods: {
+    save () {
+      console.log('Item', this.item)
+    }
   },
   computed: {
     title () {
